219d25b1bb1c448af71a221c3862e1ec8fa9ee1d
orko3012 <or.ko@hotmail.fr>
2020-03-19 14:40:07 +0100

    add message and pink mode

---
+++ contact/contact.js
@@ -0,0 +1,16 @@
+const butt = document.getElementById("button")
+butt.onclick=()=>{
+    alert("hello")
+}
+
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'pink');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+const toggler = document.querySelector('#toggler');
+toggler.addEventListener('change', switchTheme, false);
