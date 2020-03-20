019765a57ea8840347e80886f52f6fa95030fbbb
orko3012 <or.ko@hotmail.fr>
2020-03-20 11:22:30 +0100

    change h2 and add a color transition

---
+++ article/article.js
@@ -0,0 +1,11 @@
+document.querySelector(".quizz").className="quizz fush"
+
+const titre=document.getElementById("titre2")
+titre2.textContent="STOP EATING PANDA"
+
+
+const titl=document.querySelector(".titl")
+
+window.setInterval(()=>{
+    titl.classList.toggle( "red")
+}, 1000)
