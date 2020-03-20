2b8d10419a3ae6637cba09ac7c69aa69d3239b49
orko3012 <or.ko@hotmail.fr>
2020-03-20 11:33:11 +0100

    change quizz colors

--- article/article.js
+++ article/article.js
@@ -1,11 +1,15 @@
+const quizz=document.querySelectorAll(".quizz")
+for(i=0; i<quizz.length; i++){
+    quizz[i].className="quizz black"
+}
 document.querySelector(".quizz").className="quizz fush"
+
 
 const titre=document.getElementById("titre2")
 titre2.textContent="STOP EATING PANDA"
 
 
 const titl=document.querySelector(".titl")
-
 window.setInterval(()=>{
     titl.classList.toggle( "red")
 }, 1000)
