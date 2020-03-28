757034d77af958277e259d57a8670c59185aea1f
orko3012 <or.ko@hotmail.fr>
2020-03-28 07:52:24 +0100

    change button

--- contact/contact.js
+++ contact/contact.js
@@ -1,16 +1,15 @@
-const butt = document.getElementById("button")
-butt.onclick=()=>{
-    alert("Hello World !")
-}
+const butt = document.getElementById("button");
+butt.addEventListener("click", () => {
+  document.body.style.display = "none";
+});
 
 function switchTheme(event) {
-    if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'pink');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }    
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "pink");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
 }
 
-const toggler = document.querySelector('#toggler');
-toggler.addEventListener('change', switchTheme, false);
+const toggler = document.querySelector("#toggler");
+toggler.addEventListener("change", switchTheme, false);
